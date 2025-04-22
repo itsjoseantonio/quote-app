e15cae2201f388dc59174b0880aa5205a64d4786
Jose Antonio <jos.valga@gmail.com>
2025-04-21 20:05:21 -0500

    feat: Updated quotes page to get all quotes from logged user

--- src/app/(pages)/(admin)/quotes/page.tsx
+++ src/app/(pages)/(admin)/quotes/page.tsx
@@ -1,11 +1,33 @@
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { Types } from 'mongoose';
+
+import { authOptions } from '@/lib/auth';
+import { Session } from '@/types';
+import dbConnect from '@/lib/dbConnect';
+import { Quote } from '@/app/models/Quote';
+
 // ====== Components ====== //
 import QuotesForm from '@/components/forms/QuotesForm';
+import QuoteList from '@/components/forms/QuoteList';
 
-const QuotesPage = () => {
+const QuotesPage = async () => {
+    const session: Session | null = await getServerSession(authOptions);
+
+    if (!session) {
+        redirect('/');
+    }
+
+    await dbConnect();
+    const quotes = await Quote.find({
+        user: new Types.ObjectId(session.user.id),
+    }).lean();
+
     return (
         <div className='space-y-4'>
             <h1 className='text-2xl font-bold mb-3'>Manage quotes</h1>
             <QuotesForm />
+            <QuoteList quotes={quotes} />
         </div>
     );
 };
