1a637047009d76651aa58acd12eee9feebfabd32
Jose Antonio Valga <jos.valga@gmail.com>
2025-09-27 18:35:05 -0500

    feat: Added not-found page for users

--- src/app/(pages)/account/[username]/not-found.tsx
+++ src/app/(pages)/account/[username]/not-found.tsx
@@ -1,7 +1,17 @@
-'use client'
- 
+import Link from 'next/link';
+
 const NotFound = () => {
-    return <div>User not found</div>;
-}
+    return (
+        <div className='h-screen flex flex-col items-center justify-center'>
+            <div className='flex align-center justify-center mb-4 text-xl'>
+                <h1 className='mr-2 pr-2 border-r border-gray-700'>404</h1>
+                <p>User could not be found</p>
+            </div>
+            <Link href='/' className='text-sm underline decoration-1'>
+                Return home
+            </Link>
+        </div>
+    );
+};
 
 export default NotFound;
