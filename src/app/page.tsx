27a7c72740ca5d048437f44770ae84abfa5974d1
Jose Antonio Valga <jos.valga@gmail.com>
2025-10-25 21:59:38 -0500

    feat: updated banner section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 
+import banner from '@/assets/images/main-banner.jpeg';
+
 export default async function HomePage() {
     const session = await getServerSession(authOptions);
 
@@ -21,20 +23,20 @@ export default async function HomePage() {
         <>
             <Header />
             <main>
-                <section className='container mx-auto px-4 h-[calc(100vh-60px)] flex items-center justify-center bg-[url("/src/assets/images/main-banner.jpeg")]'>
+                <section className='mx-auto px-4 h-[calc(100vh-60px)] bg-[url("/images/main-banner.jpeg")] bg-cover flex items-center justify-center w-full'>
                     <div className='max-w-5xl text-center'>
-                        <h1 className='font-bold text-4xl mb-5'>
-                            Books don't change people; paragraphs do, Sometimes
+                        <h1 className='font-bold text-5xl mb-6 max-w-4xl mx-auto text-white'>
+                            Books do not change people; paragraphs do, Sometimes
                             even sentences.
                         </h1>
-                        <p className='text-xl mb-5'>
+                        <p className='text-xl mb-6 max-w-3xl mx-auto'>
                             Quotely is where you can curate and share your
                             favorite quotes with the world. Discover, inspire
                             and connect through the power of words.
                         </p>
                         <Button
                             variant='outline'
-                            className='bg-coffee text-white rounded-3xl text-2xl py-5 px-6'
+                            className='bg-coffee text-white rounded-3xl text-xl py-5 px-6 border-none'
                         >
                             <Link href='/auth/signup'>Get started</Link>
                         </Button>
