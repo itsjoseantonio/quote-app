f381f55e495c3c279fd783e60af0dedad44e86f6
Jose Antonio Valga <jos.valga@gmail.com>
2025-10-25 23:10:15 -0500

    feat: Added how it works section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,10 @@ import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 
-import banner from '@/assets/images/main-banner.jpeg';
+// ======= Assets ======== //
+import { LuUserCheck } from 'react-icons/lu';
+import { PiQuotesBold } from 'react-icons/pi';
+import { RiShareLine } from 'react-icons/ri';
 
 export default async function HomePage() {
     const session = await getServerSession(authOptions);
@@ -29,7 +32,7 @@ export default async function HomePage() {
                             Books do not change people; paragraphs do, Sometimes
                             even sentences.
                         </h1>
-                        <p className='text-xl mb-6 max-w-3xl mx-auto'>
+                        <p className='text-xl mb-6 max-w-3xl mx-auto text-white'>
                             Quotely is where you can curate and share your
                             favorite quotes with the world. Discover, inspire
                             and connect through the power of words.
@@ -40,6 +43,55 @@ export default async function HomePage() {
                         >
                             <Link href='/auth/signup'>Get started</Link>
                         </Button>
+                    </div>
+                </section>
+                <section className='bg-lightGray py-24'>
+                    <div className='text-center mb-16'>
+                        <h2 className='text-3xl font-bold mb-2'>
+                            How it works
+                        </h2>
+                        <p>
+                            Sharing your favorite quotes is just a few steps
+                            away
+                        </p>
+                    </div>
+                    <div className='grid gap-12 text-center md:grid-cols-3'>
+                        <div className='flex flex-col items-center text-richBlack'>
+                            <div className='mb-3 text-coffee w-[34px] h-[34px] flex items-center justify-center rounded-full bg-coffee/50'>
+                                <LuUserCheck fontSize={24} />
+                            </div>
+                            <h3 className='text-xl font-semibold mb-3'>
+                                1. Create your profile
+                            </h3>
+                            <p className=' max-w-xs'>
+                                Sign up and create your personalized profile to
+                                start your quote-sharing journey
+                            </p>
+                        </div>
+                        <div className='flex flex-col items-center text-richBlack'>
+                            <div className='mb-3 text-coffee w-[34px] h-[34px] flex items-center justify-center rounded-full bg-coffee/50'>
+                                <PiQuotesBold fontSize={24} />
+                            </div>
+                            <h3 className='text-xl font-semibold mb-3'>
+                                2. Add your favorite quotes
+                            </h3>
+                            <p className=' max-w-xs'>
+                                Easily add quotes from your favorites authors,
+                                books or movies to your collections.
+                            </p>
+                        </div>
+                        <div className='flex flex-col items-center text-richBlack'>
+                            <div className='mb-3 text-coffee w-[34px] h-[34px] flex items-center justify-center rounded-full bg-coffee/50'>
+                                <RiShareLine fontSize={24} />
+                            </div>
+                            <h3 className='text-xl font-semibold mb-3'>
+                                3. Share your unique Link
+                            </h3>
+                            <p className=' max-w-xs'>
+                                Share your unique link with friends and
+                                followers on any platform
+                            </p>
+                        </div>
                     </div>
                 </section>
             </main>
